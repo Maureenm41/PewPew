b8a5d6fb0082df739481143e20b050d4fdc8e99b
Maureenm41 <maureenmrchl@gmail.com>
2021-03-04 15:02:03 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const cannon = document.getElementById('cannon')
+//part 1
+//const clearColor="#121212";
+//const cannonBallColor="#000";
+//const targetMissileColor="#fff";
+const can = document.getElementById('can')
 const target = document.getElementById('target')
 const ball = document.getElementById('ball')
 
@@ -15,9 +19,59 @@ function init()
 function update()
 {
     clearCanvas(cc);
+    game.update(cc);
+    requestAnimationFrame(update);
 }
 
-function onKeydown(e)
+function onKeyDown(e)
 {
-    let keycode=e.keycode;
+    let keycode=e.keyCode;
+    
+    if(keycode==37)
+    {
+        leftkey=true;
+    }
+    
+    if(keycode==39)
+    {
+        rightkey=true;
+    }
+    
+    if(keycode==32)
+    {
+        game.cannonShootBall();
+    }
 }
+
+function onKeyUp(e)
+{
+    let keycode=e.keyCode;
+    
+    if(keycode==37){
+        leftkey=false;
+    }
+    
+    if(keycode==39){
+        rightkey=false;
+    }
+}
+
+window.onload=function(){
+    init();
+    update();
+}
+
+//part 2
+
+let game = function()
+    {
+        let _ =this ;
+        this.scl=0.1;
+        this.cannon_img = document.getElementById("cannon");
+        this.target_img = document.getElementById("target");
+        this.cd={w:384*_.scl , h:512*_.scl}; // cd = cannon data
+        this.td={}; //td =target data
+    }
+
+
+    
