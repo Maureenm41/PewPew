148d90dfe2b58ccc5d85ea932601ab2314a09d8e
Maureenm41 <maureenmrchl@gmail.com>
2021-03-04 18:04:26 +0100

    update

--- script.js
+++ script.js
@@ -66,12 +66,52 @@ window.onload=function(){
 let game = function()
     {
         let _ =this ;
+
         this.scl=0.1;
+
         this.cannon_img = document.getElementById("cannon");
+
         this.target_img = document.getElementById("target");
+
         this.cd={w:380*_.scl , h:400*_.scl}; // cd = cannon data
+
         this.td={w:400*_.scl , h:400*_.scl , rows: 3 , cols: 8 ,gap : 10}; //td =target data
-        this.cannon={x:cw/2 };
+
+        this.cannon={x:cw/2-_.sd.w/2 , y:ch-10-_.sd.h, w:sd.w , h:_.sd.h };
+
+        this.ball = new Array();
+
+        this.cannonSpeed = 5 ;
+
+        this.ballSpeed = 3.5 ; 
+
+        this.target = new Array();
+
+        this.targetMissiles = new Array();
+
+        this.targetMissilesSpeed = 3.5 ; 
+
+        this.missilesWidth = 4 ;
+
+        this.missilesHeight = 25 ; 
+
+        this.targetRange = { x:0 , w:0 } ; 
+
+        this.completed = false ;
+
+
+        this.init = function()
+            {
+                this.draw = function(cc)
+                    {
+
+                    }
+
+                this.update = function(cc)
+                    {
+
+                    }
+            }
     }
 
 
